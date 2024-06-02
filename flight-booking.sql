2670e14ef8158d4f4ddfc89c5e9cdd4c3fc5aeff
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-06-03 00:44:01 +0530

    new commit

---
+++ flight-booking.sql
@@ -0,0 +1,14 @@
+create database flightdb;
+use flightdb;
+show tables;
+select * from flights;
+
+create database bookingdb;
+use bookingdb;
+show tables;
+select * from bookings;
+
+create database paymentdb;
+use paymentdb;
+show tables;
+select * from payment_details;
