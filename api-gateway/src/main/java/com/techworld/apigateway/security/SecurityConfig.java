736c72000389eabd1a7703d23b67bcad498aba80
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-06-07 01:49:47 +0530

    new commit

--- api-gateway/src/main/java/com/techworld/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/techworld/apigateway/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.techworld.apigateway.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -20,9 +21,21 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeExchange()
+
+                // Public endpoints
                     .pathMatchers("/auth/users/**").permitAll()
                     .pathMatchers("/auth/roles/**").permitAll()
+
+                // User role endpoints
                     .pathMatchers("/flight-search-service/v1/api/search/**").hasRole(USER)
+                    .pathMatchers(HttpMethod.GET, "/flight-service/v1/api/flights/**").hasRole(USER)
+                    .pathMatchers(HttpMethod.PUT, "/flight-service/v1/api/flights/**").hasRole(USER)
+                    .pathMatchers("/booking-service/v1/api/flights/**").hasRole(USER)
+
+                // Admin role endpoints
+                    .pathMatchers(HttpMethod.POST, "/flight-service/v1/api/flights/**").hasRole(ADMIN)
+
+                // Any other request must be authenticated
                 .anyExchange().authenticated()
                 .and()
                 .oauth2ResourceServer()
