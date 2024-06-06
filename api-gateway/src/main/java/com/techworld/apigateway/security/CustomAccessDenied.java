9400614e42d104baf232b8294df54977cbd3aa22
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-06-07 02:31:31 +0530

    new commit

--- api-gateway/src/main/java/com/techworld/apigateway/security/CustomAccessDenied.java
+++ api-gateway/src/main/java/com/techworld/apigateway/security/CustomAccessDenied.java
@@ -1,19 +1,12 @@
 package com.techworld.apigateway.security;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Map;
 
 @Component
 public class CustomAccessDenied implements ServerAccessDeniedHandler {
--- api-gateway/src/main/java/com/techworld/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/techworld/apigateway/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
 
 @Configuration
 @EnableWebFluxSecurity
