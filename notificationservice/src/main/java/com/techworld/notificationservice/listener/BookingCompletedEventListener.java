3b9d73df16530a5bd452f9a59a420640a7ec841d
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-08-17 01:00:53 +0530

    new commit

--- notificationservice/src/main/java/com/techworld/notificationservice/listener/BookingCompletedEventListener.java
+++ notificationservice/src/main/java/com/techworld/notificationservice/listener/BookingCompletedEventListener.java
@@ -7,15 +7,17 @@ import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class BookingCompletedEventListener {
 
     private final ObservationRegistry observationRegistry;
     private final Tracer tracer;
 
-    @KafkaListener(topics = "notificationTopic")
+    @KafkaListener(topics = "notificationTopic", groupId = "notificationId")
     public void handleNotification(BookingCompletedEvent bookingCompletedEvent) {
         Observation.createNotStarted("on-message", this.observationRegistry).observe(() -> {
             log.info("Got message <{}>", bookingCompletedEvent);
